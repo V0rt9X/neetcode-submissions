651582111a3e3d28607da2770fd60266ed690dc8
V0rt9X <236027588+V0rt9X@users.noreply.github.com>
2026-09-09 17:30:07 +0200

    Add: surrounded-regions - submission-6

---
+++ Data Structures & Algorithms/surrounded-regions/submission-6.py
@@ -0,0 +1,31 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
+        rows, cols = len(board), len(board[0])
+        
+        def dfs(r, c):
+            if (r not in range(rows) or
+                c not in range(cols) or
+                board[r][c] != 'O'):
+                return
+            
+            board[r][c] = "T"
+
+            for dr, dc in directions:
+                dfs(dr + r, dc + c)
+
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "O" and r in (0, rows - 1) or c in (0, cols - 1):
+                    dfs(r, c)
+        
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+        
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "T":
+                    board[r][c] = "O"
+        
