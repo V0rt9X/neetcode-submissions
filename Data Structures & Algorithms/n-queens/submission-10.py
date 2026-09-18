9ab33d1fb303e3385da8fb9177f649c2a650c7a2
V0rt9X <236027588+V0rt9X@users.noreply.github.com>
2026-09-18 21:34:40 +0200

    Add: n-queens - submission-10

---
+++ Data Structures & Algorithms/n-queens/submission-10.py
@@ -0,0 +1,32 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [['.'] * n for _ in range(n)]
+        res = []
+
+        col = set()
+        posDiag = set()
+        negDiag = set()
+
+        def backtrack(r):
+            if r == n:
+                res.append(["".join(board[r]) for r in range(n)])
+                return
+            
+            for c in range(n):
+                if c in col or r + c in posDiag or r - c in negDiag:
+                    continue
+                
+                board[r][c] = 'Q'
+                col.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                backtrack(r + 1)
+                col.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+                board[r][c] = '.'
+        
+        backtrack(0)
+        return res
+
+        # T: O(n! * n^2) S: O(n^2)
