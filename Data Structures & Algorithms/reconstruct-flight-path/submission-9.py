d8239406366545463f5bc0c04b80f3cc19caa13c
V0rt9X <236027588+V0rt9X@users.noreply.github.com>
2026-09-23 21:33:59 +0200

    Add: reconstruct-flight-path - submission-9

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-9.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        adj = defaultdict(list)
+
+        for src, dst in sorted(tickets, reverse = True):
+            adj[src].append(dst)
+        
+        res = []
+        
+        def topol(src):
+            while adj[src]:
+                dst = adj[src].pop()
+                topol(dst)
+            
+            res.append(src)
+        
+        topol("JFK")
+        return res[::-1]
+
+        # O(v * Cpop + e * Cpush) -> T: O(e log e), S: O(v + e)
